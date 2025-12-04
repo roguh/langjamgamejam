596e10f9875ad981af0398d6d4c9894242edb0ea
Felina Rivera Calzadillas (ROGUH) <felina.rivera@powerflex.com>
2025-12-03 23:08:43 -0700

    Add basic Scheme parser (Parsec)

--- practice/haskelloni48/app/Main.hs
+++ practice/haskelloni48/app/Main.hs
@@ -1,6 +1,17 @@
 module Main (main) where
 
-import Lib
+import System.Environment
+import qualified Data.Text as T
+
+import Parser
+import LispVal
+
 
 main :: IO ()
-main = someFunc
+main = do
+    -- TODO better cli UX
+    (expr:_) <- getArgs
+    -- TODO use fancy monad magic?
+    case Parser.readExpr (T.pack expr) of
+        Left err -> putStrLn (show err)
+        Right val -> putStrLn (T.unpack (showVal val))
---
+++ practice/haskelloni48/src/Parser.hs
@@ -0,0 +1,115 @@
+{-# LANGUAGE OverloadedStrings #-}  
+
+module Parser (
+    readExpr,
+    readExprFile
+) where
+
+import LispVal
+import Text.Parsec
+import Text.Parsec.Text
+import qualified Text.Parsec.Token as Tok
+import qualified Text.Parsec.Language as Lang
+import qualified Data.Text as T
+import Data.Functor.Identity (Identity)
+import Control.Monad (mzero)
+
+import Data.Functor (($>))
+import Data.Char (digitToInt)
+
+
+-- Monad protips:
+-- m x $> f      -- apply function f to contents x
+-- m z *> m x    -- overwrite contents of first monad z with x
+-- m1 x <|> m2 y -- try to apply m1, if that fails try m2; for Parser.Parser this tries one parser, then another in case the first fails
+-- f <$> m x     -- apply function f to contents x
+-- TODO <?>
+-- TODO <$
+-- TODO <*
+
+-- Tokenizer
+lexer :: Tok.GenTokenParser T.Text () Identity
+lexer = Tok.makeTokenParser $ Lang.emptyDef {
+    Tok.commentStart = "{-"
+    , Tok.commentEnd = "-}"
+    , Tok.commentLine = "--"
+    , Tok.opStart = mzero
+    , Tok.opLetter = mzero
+    , Tok.identStart = letter <|> oneOf "!$%&*/:<=>?^_~"
+    , Tok.identLetter = digit <|> letter <|> oneOf "!$%&*/:<=>?^_~+-.@"
+}
+
+whitespace :: Parser ()
+whitespace = Tok.whiteSpace lexer
+parens :: Parser a -> Parser a
+parens = Tok.parens lexer
+lexeme :: Parser a -> Parser a
+lexeme = Tok.lexeme lexer
+quoted :: Parser a -> Parser a
+quoted p = try (char '\'') *> p
+identifier :: Parser T.Text
+identifier = T.pack <$> (Tok.identifier lexer <|> specialIdentifier) <?> "identifier"
+    where specialIdentifier = lexeme $ try $
+            string "-" <|> string "+" <|> string "..."
+
+
+type Radix = (Integer, Parser Char)
+
+numberWithRadix :: Radix -> Parser Integer
+numberWithRadix (base, baseDigit) = do
+    digits <- many1 baseDigit
+    let n = foldl' (\x d -> base * x + toInteger (digitToInt d)) 0 digits
+    seq n (return n)
+
+decimal :: Parser Integer
+decimal = Tok.decimal lexer
+
+sign :: Parser (Integer -> Integer)
+sign =   char '-' $> negate
+     <|> char '+' $> id
+     <|> return id
+
+intRadix :: Radix -> Parser Integer
+intRadix r = sign <*> numberWithRadix r
+
+textLiteral :: Parser T.Text
+textLiteral = T.pack <$> Tok.stringLiteral lexer
+
+nil :: Parser ()
+nil = try (char '\'' *> string "()") *> return () <?> "nil"
+
+hashVal :: Parser LispVal
+hashVal = lexeme $ char '#'
+    *> (char 't' $> Bool True
+    <|> char 'f' $> Bool False
+    <|> char 'b' *> (Number <$> intRadix (2, oneOf "01"))
+    <|> char 'o' *> (Number <$> intRadix (8, octDigit))
+    <|> char 'd' *> (Number <$> intRadix (10, digit))
+    <|> char 'x' *> (Number <$> intRadix (16, hexDigit))
+    <|> oneOf "ei" *> fail "Unsupported: exactness"
+    <|> char '(' *> fail "Unsupported: vector"
+    <|> char '\\' *> fail "Unsupported: char")
+
+lispVal :: Parser LispVal
+lispVal = hashVal
+    <|> Nil <$ nil
+    <|> Number <$> try (sign <*> decimal)
+    <|> Atom  <$> identifier
+    <|> String <$> textLiteral
+    <|> _Quote <$> quoted lispVal
+    <|> List <$> parens manyLispVal
+
+manyLispVal :: Parser [LispVal]
+manyLispVal = lispVal `sepBy` whitespace
+
+_Quote :: LispVal -> LispVal
+_Quote x = List [Atom "quote", x]
+
+contents :: Parser a -> Parser a
+contents p = whitespace *> lexeme p <* eof
+
+readExpr :: T.Text -> Either ParseError LispVal
+readExpr = parse (contents lispVal) "<stdin>"
+
+readExprFile :: SourceName -> T.Text -> Either ParseError LispVal
+readExprFile = parse (contents (List <$> manyLispVal))
