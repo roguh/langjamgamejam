09f4979aa4e28042f709fd851216b5548eefbf7e
Felina Rivera Calzadillas (ROGUH) <felina.rivera@powerflex.com>
2025-12-16 03:13:25 -0700

    Improve platform movement

--- practice/platformer1/src/main.ts
+++ practice/platformer1/src/main.ts
@@ -4,7 +4,7 @@ import kaplay from "kaplay";
 // Use k. prefix anyway to get better autocomplete
 const k = kaplay();
 
-import { pickAnim } from "./util";
+import { pickAnim, tileMaker } from "./util";
 import { saveTime, loadGame, fastSaveGame } from "./save";
 import { makeTextButton } from "kaplay-ui/inputs";
 
@@ -16,8 +16,26 @@ const [PW, PH] = [CW, CH];
 k.loadRoot("./"); // A good idea for Itch.io publishing later
 k.loadSprite("bean", "sprites/bean.png");
 k.loadSpriteAtlas("sprites/map.png", {
-  floor: { x: 0, y: 0, width: TW, height: TH },
-  block: { x: TW, y: 0, width: TW, height: TH },
+  floor_rightdown: { x: 0, y: 0, width: TW, height: TH },
+  floor_rightup: { x: TW, y: 0, width: TW, height: TH },
+  floor: { x: TW * 2, y: 0, width: TW, height: TH },
+  floor_vert: { x: TW * 3, y: 0, width: TW, height: TH },
+  flowy: { x: 0, y: TH, width: TW, height: TH },
+  flowy_denser: { x: TW, y: TH, width: TW, height: TH },
+  flowy_densest: { x: TW * 2, y: TH, width: TW, height: TH },
+  coin: {
+    x: 0,
+    y: TH * 3,
+    sliceX: 4,
+    width: TW * 4,
+    height: TH,
+    anims: {
+      shine: {
+        from: 0,
+        to: 3,
+      },
+    },
+  },
 });
 
 k.loadSpriteAtlas("sprites/sama.png", {
@@ -29,7 +47,7 @@ k.loadSpriteAtlas("sprites/sama.png", {
     sliceX: 11,
     anims: {
       idle: { from: 0, to: 0 },
-      move: { from: 1, to: 8, loop: true, pingpong: false, speed: 12 },
+      move: { from: 1, to: 8, loop: true, pingpong: false, speed: 15 },
       jump: { from: 9, to: 9 },
       fall: { from: 10, to: 10 },
     },
@@ -42,7 +60,7 @@ k.loadSpriteAtlas("sprites/sama.png", {
     sliceX: 11,
     anims: {
       idle: { from: 0, to: 0 },
-      move: { from: 0, to: 5, loop: true, pingpong: false, speed: 12 },
+      move: { from: 0, to: 5, loop: true, pingpong: false, speed: 6 },
     },
   },
   sama_roll: {
@@ -53,43 +71,51 @@ k.loadSpriteAtlas("sprites/sama.png", {
     sliceX: 11,
     anims: {
       idle: { from: 0, to: 0 },
-      move: { from: 0, to: 11, loop: true, pingpong: false, speed: 15 },
+      move: { from: 0, to: 11, loop: true, pingpong: false, speed: 6 },
     },
   },
 });
 
 const map = k.addLevel(
   [
-    "5                                                     5",
-    "5                                                     5",
-    "5   111                  111                  111     5",
-    "5        111                                          5",
-    "5                                   111               5",
-    "5   111              111                              5",
-    "5             111                                     5",
-    "5111111                      111           111        5",
-    "5155555                                               5",
-    "5155555   111                                         5",
-    "5155551111111111111111111111111111111111111111111111115",
-    "5155555555555555555555555555555555555555555555555555115",
+    "C                                                     w",
+    "w                  5                  5                  W                w",
+    "w            5                                                            w",
+    "w    5                   W                    5                           w",
+    "w                                                           5             w",
+    "w                 5                 5                                     w",
+    "w                                                 5      5                w",
+    "w                                                                         w",
+    "w       5                        5                              5         w",
+    "w                         5                                               w",
+    "w                                        5                                w",
+    "w                    5                                     5              w",
+    "w                                                                         w",
+    "w                         C              5                          C     w",
+    "w          C     5                                                        w",
+    "w                                                         C               w",
+    "w   012                  01111111112     5     2                  01111111w",
+    "w        012          C                                                   w",
+    "w    C                                                 0111112            w11111111111112",
+    "w   012             01112                                                 w             11112",
+    "w             012                 5                             C         w                 11111111112",
+    "w111111                                         01112        0111112      w",
+    "w1wwwww         C                      5                                  w",
+    "w1wCWww   111                                    C                        w",
+    "w1wWWw11111111111111111111111111111111111111111111111111111111111111111111w",
+    "w1wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww11w",
   ],
   {
     tileWidth: TW,
     tileHeight: TH,
     tiles: {
-      1: () => [
-        "solid",
-        k.sprite("floor"),
-        k.area(),
-        k.body({ isStatic: true }),
-      ],
-      5: () => [
-        "solid",
-        k.sprite("block"),
-        k.area(),
-        k.body({ isStatic: true }),
-        { health: 100 },
-      ],
+      0: tileMaker("floor_rightup"),
+      1: tileMaker("floor"),
+      2: tileMaker("floor_rightdown"),
+      5: tileMaker("floor_vert"),
+      w: tileMaker("flowy", 100),
+      W: tileMaker("flowy_densest", 100),
+      C: tileMaker("coin", 1, "shine"),
     },
   },
 );
@@ -102,7 +128,7 @@ const txt = k.add([
   {
     lastUpdate: 0,
     update: () => {
-      if (k.time() - txt.lastUpdate > 1) {
+      if (k.time() - txt.lastUpdate > 0.1) {
         const fps = (1 / k.dt()).toFixed(1);
         txt.text = `FPS: ${fps} ${player.pos.x.toFixed(1)},${player.pos.y.toFixed(1)}`;
         txt.lastUpdate = k.time();
@@ -119,29 +145,32 @@ const player = k.add([
   // TODO anchor point stays static when rolling
   k.anchor("center"),
   k.body(),
-  k.doubleJump(),
   k.pos(),
   {
-    initPos: k.vec2(300, 80),
-    speed: 200,
-    jump_force: 700,
+    initPos: k.vec2(100, 100),
+    speed: 350,
+    rollSpeed: 300,
+    runSpeed: 450,
+    lastGrounded: 0,
+    jump_force: 800,
     direction: "right",
     isRolling: false,
     mineSpeed: 20,
     baseSprite: "sama",
     rollSprite: "sama_sneak",
-    shape: new k.Rect(k.vec2(0, (PH * (1 - 0.7)) / 2), PW / 2, PH * 0.7),
-    rollShape: new k.Rect(k.vec2(0, (PH * (1 - 0.45)) / 2), PW / 2, PH * 0.45),
+    shape: new k.Rect(k.vec2(0, (PH * (1 - 0.6)) / 2), PW / 4, PH * 0.6),
+    rollShape: new k.Rect(k.vec2(0, (PH * (1 - 0.4)) / 2), PW / 4, PH * 0.4),
     roll: (v: boolean) => {
       // TODO Avoid collision when standing up
       player.isRolling = v;
+      player.speed = player.isRolling ? player.rollSpeed : player.runSpeed;
       player.area.shape = player.isRolling ? player.rollShape : player.shape;
     },
     canMine: () => player.isRolling,
     init: () => {
       player.area.shape = player.shape;
       player.moveTo(player.initPos);
-      pickAnim(player);
+      pickAnim(player, "idle");
     },
     toSaveFile: () => ({
       pos: { x: player.pos.x, y: player.pos.y },
@@ -158,22 +187,38 @@ const player = k.add([
 
 // Initialization
 k.onLoad(() => {
-  map.use(k.scale(0.95));
+  map.use(k.scale(0.45));
   player.init();
   k.setCamScale(1.5);
   loadGame(player);
+  // k.debug.inspect = true;
+  k.debug.log("beeeeeeeeeeeeeeep");
+  k.debug.showLog = true;
+  k.debug.log("booooooooooop");
+  k.debug.log("booooooooooop");
+  k.debug.log("booooooooooop");
+  k.debug.log("booooooooooop");
+  k.debug.log("booooooooooop");
+  k.debug.log({ 1: 2, a: [[[[[[[[[1]]]]]]]]] });
 });
 
+// TODO: add action buffer for attacks and jumps
+// when action is ready, play the buffered sequence
 const actions = {
   lastDown: 0,
   lastUp: 0,
   reset: () => player.moveTo(player.initPos),
   up: () => {
     console.debug("action.up");
-    // TODO allow holding the up button to double jump the maximum height...
-    player.doubleJump(player.jump_force);
-    pickAnim(player);
+    // Allow coyote time late jumps
+    if (player.isGrounded() || k.time() - player.lastGrounded < 0.5)
+      player.jump(player.jump_force);
+    pickAnim(player, "jump");
     actions.lastUp = k.time();
+  },
+  upStop: () => {
+    // Allow finer jump controls by stopping on release
+    if (player.vel.y < 0) player.vel.y = 0;
   },
   left: () => {
     console.debug("action.left");
@@ -181,11 +226,13 @@ const actions = {
     pickAnim(player, "move");
     player.flipX = true;
   },
-  leftStop: () => pickAnim(player),
+  leftStop: () => pickAnim(player, "idle"),
+  // TODO: add action buffer for attacks and jumpos
+  // when action is ready, play the buffered sequence
   downStart: () => {
     console.debug("action.downStart");
     player.roll(!player.isRolling);
-    pickAnim(player);
+    pickAnim(player, "idle");
     actions.lastDown = k.time();
   },
   right: () => {
@@ -194,7 +241,7 @@ const actions = {
     pickAnim(player, "move");
     player.flipX = false;
   },
-  rightStop: () => pickAnim(player),
+  rightStop: () => pickAnim(player, "idle"),
   clickOrTouchStart: (pos) => {
     console.debug("action.clickOrTouchStart");
     if (pos.dist(txt.pos) < txt.width) {
@@ -218,6 +265,7 @@ const actions = {
   },
 };
 
+// TODO completeness
 k.onKeyPress("0", actions.reset);
 k.onKeyPress("up", actions.up);
 k.onKeyDown("left", actions.left);
@@ -227,8 +275,10 @@ k.onKeyPress("w", actions.up);
 k.onKeyDown("a", actions.left);
 k.onKeyPress("s", actions.downStart);
 k.onKeyDown("d", actions.right);
+k.onKeyRelease("w", actions.upStop);
 k.onKeyRelease("a", actions.leftStop);
 k.onKeyRelease("d", actions.rightStop);
+k.onKeyRelease("up", actions.upStop);
 k.onKeyRelease("left", actions.leftStop);
 k.onKeyRelease("right", actions.rightStop);
 k.onMousePress(() => actions.clickOrTouchStart(k.mousePos()));
@@ -237,11 +287,6 @@ if (k.isTouchscreen()) {
   k.onTouchStart(actions.clickOrTouchStart);
   k.onTouchMove(actions.clickOrTouch);
 }
-
-k.onCollide("mover", "solid", (a, b) => {
-  // Stop jump animation if on ground
-  pickAnim(player);
-});
 
 k.onCollideUpdate("player", "solid", (a, b, col) => {
   const canMine = a.canMine() && b.health !== undefined && col?.normal.y === 0;
@@ -274,9 +319,27 @@ k.onUpdate(() => {
   txt.update();
   k.setCamPos(player.pos.x, player.pos.y);
 
+  // Destroy random tile, starting near x=0
+  const maxX = k.time() / 10 + 1;
+  if (k.time() % 1 < 0.1 && k.time() < 5)
+    map.getSpatialMap().map((row, y) =>
+      row.map((tile, x) => {
+        // Destroy random tile, starting near x=0 edge of map
+        if (x < maxX && Math.random() < 0.01) tile.destroy();
+      }),
+    );
+
   // Fall off map?
   if (player.pos.y > 10000) {
     actions.reset();
+  }
+  if (player.isGrounded()) {
+    player.lastGrounded = k.time();
+  }
+  if (player.isFalling()) {
+    player.gravityScale = 2.5;
+  } else {
+    player.gravityScale = 1;
   }
   // Save every 1 second
   if (k.time() - saveTime() > 1) {
--- practice/platformer1/src/util.ts
+++ practice/platformer1/src/util.ts
@@ -1,16 +1,34 @@
 import kaplay from "kaplay";
 
 const k = kaplay();
-export const pickAnim = (entity, animName = "idle") => {
+export const pickAnim = (entity, animName: string) => {
   if (entity.isJumping()) animName = "jump";
   if (entity.isFalling()) animName = "fall";
   const oldFlipX = entity.flipX;
+
+  // Switch sprites?
   if (entity.isRolling && entity.sprite !== entity.rollSprite)
     entity.use(k.sprite(entity.rollSprite));
   if (!entity.isRolling && entity.sprite !== entity.baseSprite)
     entity.use(k.sprite(entity.baseSprite));
-  if (!entity.hasAnim(animName)) entity.use(k.sprite(entity.baseSprite));
+
+  if (!entity.hasAnim(animName)) {
+    entity.use(k.sprite(entity.baseSprite));
+  }
+
+  // Preserve Direction if switching sprites
   entity.flipX = oldFlipX;
+
+  // Only change if necessary
   if (entity.curAnim() != animName) entity.play(animName);
-  console.log(entity.curAnim(), entity.sprite);
 };
+
+export const tileMaker =
+  (name: string, health: number = undefined, anim: string = undefined) =>
+  () => [
+    "solid",
+    k.sprite(name),
+    k.area(),
+    k.body({ isStatic: true }),
+    { health },
+  ];
